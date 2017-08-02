1d83a7cd0683855ca9ae0d502a0b045b83bca164
eline-91 <elinevanelburg@hotmail.com>
2017-08-02 14:23:58 +0000

    Added function to update basemap, checking for illogical changes.

--- RCode/src/changeDetectionFunctions.R
+++ RCode/src/changeDetectionFunctions.R
@@ -7,7 +7,9 @@ detect_change = function(pvalueRaster, threshold, outputName) {
   values(changed)[values(changed) >= threshold] = NA
   values(changed)[is.na(values(changed)) == FALSE] = 1
   
-  writeRaster(changed, filename = outputName)
+  if (!is.null(outputName)) {
+    writeRaster(changed, filename = outputName)
+  }
   
   return(changed)
 }
@@ -47,19 +49,66 @@ predict_changedPixels = function(metricData, changed_mask, rf_model, outputName,
   }
 }
 
-type_of_change = function() {
+update_basemap = function(basemap, predicted, cores) {
   
+  b = brick(basemap, predicted)
+  mat = get_illogicalChanges()
   
+  checker = function(x) {
+    # Situation 1: basemap is NA, predicted is not > change to predicted
+    # Situation 2: basemap has value, predicted is NA > keep basemap value
+    # Situation 3: both have values, check illogical changed matrix
+    # Situation 4: both are NA, keep NA
+    
+    if (is.na(x[1]) && !is.na(x[2])) {
+      updated=x[2]
+    } else if (!is.na(x[1]) && is.na(x[2])) {
+      updated=x[1]
+    } else if (is.na(x[1]) && is.na(x[2])) {
+      updated=NA
+    } else if (!is.na(x[1]) && !is.na(x[2])){
+      illog = mat[toString(x[1]), toString(x[2])]
+      if (illog == 1) { # illogical transition, keep basemap value
+        updated = x[1]
+      } else if (illog == 0) { # possible transition, update basemap value
+        updated = x[2]
+      } else {
+        updated = 99 #temp
+      }
+    }
+    
+    return(updated)
+  }
+  
+  beginCluster(cores, nice = min(cores - 1, 19))
+  newMap = clusterR(b, calc, args=list(fun=checker), export='mat')
+  #newMap = calc(b, fun = checker)
+  endCluster()
+  
+  return(newMap)
 }
 
-check_logicality = function() {
-  
-  
-}
-
-
-r <- raster(ncols=36, nrows=18)
-r[] <- 1:ncell(r)/5000
-head(r)
-ch = detect_change(r, 0.05)
-head(ch)
+# r1 = raster(nrows=4, ncols=4)
+# vals = rep(c(10,20,30,40),4)
+# vals[2] = NA
+# vals[7] = NA
+# vals[13] = 10
+# vals[14] = 20
+# vals[15] = 60
+# vals[16] = 80
+# values(r1) = vals
+# head(r1)
+# 
+# r2 = r1
+# vals2 = vals
+# vals2[7] = 50
+# vals2[3] = NA
+# vals2[13] = 10
+# vals2[14] = 80
+# vals2[15] = 40
+# vals2[16] = 30
+# values(r2) = vals2
+# head(r2)
+# 
+# r3 = update_basemap(r1, r2, 4)
+# head(r3)
