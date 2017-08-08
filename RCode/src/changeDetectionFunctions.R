4df2286d5451788bb85f933e0f1b343a9092e659
eline-91 <elinevanelburg@hotmail.com>
2017-08-08 12:45:32 +0000

    Working version of the functions to detect change and update basemap.

--- RCode/src/changeDetectionFunctions.R
+++ RCode/src/changeDetectionFunctions.R
@@ -1,6 +1,8 @@
 library(raster)
-source("utils/loadData.R")
+library(ranger)
+library(tools)
 source("utils/loadInfo.R")
+source("utils/SetTempPath.R")
 
 detect_change = function(pvalueRaster, threshold, outputName) {
   changed = pvalueRaster
@@ -8,7 +10,7 @@ detect_change = function(pvalueRaster, threshold, outputName) {
   values(changed)[is.na(values(changed)) == FALSE] = 1
   
   if (!is.null(outputName)) {
-    writeRaster(changed, filename = outputName)
+    writeRaster(changed, filename = outputName, overwrite=TRUE)
   }
   
   return(changed)
@@ -16,10 +18,7 @@ detect_change = function(pvalueRaster, threshold, outputName) {
 
 predict_changedPixels = function(metricData, changed_mask, rf_model, outputName, cores) {
   # Load harmonic metrics of the third year
-  harmonics = load_harmonicMetrics(metricData)
-  
-  changed_harmonics = mask(harmonics, changed_mask)
-  rfmodel = readRDS(rf_modelPath)
+  changed_harmonics = mask(metricData, changed_mask)
   
   RP = function(...)
   {
@@ -30,7 +29,7 @@ predict_changedPixels = function(metricData, changed_mask, rf_model, outputName,
   
   if (!file.exists(outputName)) {
     print("Predicting...")
-    print(system.time(predicted = predict(changed_harmonics, rfmodel, fun=RP, num.threads=cores,
+    print(system.time(predicted <- predict(changed_harmonics, rf_model, fun=RP, num.threads=cores,
                                           progress="text")))
     
     # Print unique values of predicted raster
@@ -49,12 +48,17 @@ predict_changedPixels = function(metricData, changed_mask, rf_model, outputName,
   }
 }
 
+
 update_basemap = function(basemap, predicted, cores) {
+  
+  # For timing purposes
+  print(paste0("Start time: ", Sys.time()))
   
   b = brick(basemap, predicted)
   mat = get_illogicalChanges()
   
   checker = function(x) {
+    
     # Situation 1: basemap is NA, predicted is not > change to predicted
     # Situation 2: basemap has value, predicted is NA > keep basemap value
     # Situation 3: both have values, check illogical changed matrix
@@ -73,42 +77,16 @@ update_basemap = function(basemap, predicted, cores) {
       } else if (illog == 0) { # possible transition, update basemap value
         updated = x[2]
       } else {
-        updated = 99 #temp
+        updated = 99 # To check for unaccounted for situations (value should not be present!)
       }
     }
-    
     return(updated)
   }
   
-  beginCluster(cores, nice = min(cores - 1, 19))
-  newMap = clusterR(b, calc, args=list(fun=checker), export='mat')
-  #newMap = calc(b, fun = checker)
-  endCluster()
+  newMap = calc(b, fun = checker)
+  
+  # For timing purposes
+  print(paste0("End time: ", Sys.time()))
   
   return(newMap)
 }
-
-# r1 = raster(nrows=4, ncols=4)
-# vals = rep(c(10,20,30,40),4)
-# vals[2] = NA
-# vals[7] = NA
-# vals[13] = 10
-# vals[14] = 20
-# vals[15] = 60
-# vals[16] = 80
-# values(r1) = vals
-# head(r1)
-# 
-# r2 = r1
-# vals2 = vals
-# vals2[7] = 50
-# vals2[3] = NA
-# vals2[13] = 10
-# vals2[14] = 80
-# vals2[15] = 40
-# vals2[16] = 30
-# values(r2) = vals2
-# head(r2)
-# 
-# r3 = update_basemap(r1, r2, 4)
-# head(r3)
