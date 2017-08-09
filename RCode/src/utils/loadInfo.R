15309ba508362e819c263d723a360c8b5c3ebe5d
eline-91 <elinevanelburg@hotmail.com>
2017-08-09 16:23:29 +0000

    Adapted these functions to use the configuration file to obtain information and then pass it through.

--- RCode/src/utils/loadInfo.R
+++ RCode/src/utils/loadInfo.R
@@ -1,15 +1,18 @@
 # Script that contains functions to load information into functions.
+# Information is taken from the configuration.yaml file that is located in
+# /home/eline91/shared/Code/RCode/
+
+library(yaml)
+config = "/home/eline91/shared/Code/RCode/configuration.yaml"
+info = yaml.load_file(config)
 
 get_reclassMatrix = function(){
   # Function to return matrix to reclassify predicted raster to the proper codes.
-  data = c(1, 60, # Bareland
-           2, 40, # Cropland
-           3, 10, # Forest
-           4, 30, # Grassland
-           5, 20, # Shrubland
-           6, 50, # Urban
-           7, 80) # Water
-  mat = matrix(data = data, nrow=7, ncol=2, byrow=T)
+  codes = get_codes()
+  order = c(seq(1:length(codes)))
+  
+  mat = matrix(data = c(order, codes), nrow = 7, ncol = 2)
+  
   return(mat)
 }
 
@@ -18,15 +21,9 @@ get_illogicalChanges = function() {
   # 1 = illogical change
   # 0 = possible change
   
-  data = c(0,1,1,1,1,0,1,
-           0,0,0,0,0,0,1,
-           1,0,0,0,0,0,1,
-           1,1,0,0,0,0,1,
-           1,1,0,0,0,0,1,
-           1,1,0,0,0,0,0,
-           1,1,1,1,1,0,0)
-  rcNames = c(10,20,30,40,50,60,80)
-  
+  data = info$changeDetection$changeMatrix
+  rcNames = info$changeDetection$rcNames
+
   mat = matrix(data = data, nrow=7, ncol=7, byrow=T)
   rownames(mat) = rcNames
   colnames(mat) = rcNames
@@ -34,27 +31,40 @@ get_illogicalChanges = function() {
   return(mat)
 }
 
+# TODO: should become get_classes
 get_legend = function() {
-  legend=c("Bareland", "Cropland", "Forest", "Grassland", "Shrubland", "Urban", "Water")
-  return(legend)
+  #legend=c("Bareland", "Cropland", "Forest", "Grassland", "Shrubland", "Urban", "Water")
+  classes = info$classification$classes
+  return(classes)
 }
 
 get_colors = function() {
-  colors=c("khaki4", "gold", "darkgreen", "lawngreen", "purple", "red", "blue")
+  #colors=c("khaki4", "gold", "darkgreen", "lawngreen", "purple", "red", "blue")
+  colors = info$classification$colors
   return(colors)
 }
 
+get_codes = function() {
+  codes = info$classification$codes
+  return(codes)
+}
+
 get_bandPattern = function() {
-  bandPattern = "NDVI_sm_ts.tif$"
+  bandPattern = info$dataset$bandPattern
   return(bandPattern)
 }
 
 get_tile = function() {
-  tile = "X16Y06"
+  tile = info$dataset$tile
   return(tile)
 }
 
 get_tempDir = function() {
-  tempDir = "../../../userdata3/tmp/"
+  tempDir = info$folders$temp
   return(tempDir)
+}
+
+get_inputDir = function() {
+  inputDir = info$folders$NDVI_images
+  return(inputDir)
 }
